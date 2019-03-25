0af1a82165a2d4fb8f7f51f72e5fd63c66b87ccf
ajay <16p102@kce.ac.in>
2019-03-25 12:43:46 +0530

    initial commit

--- SinkleLinkedList.py
+++ SinkleLinkedList.py
@@ -51,6 +51,7 @@ class SingleLinkedList:
         self.start=temp
     
     def insert_at_end(self,data):
+        
         temp = Node(data)
         if self.start is None:
             self.start=temp
@@ -61,8 +62,8 @@ class SingleLinkedList:
         p.link=temp
 
     def create_list(self):
-        for _ in range(int(input("Enter no of nodes"))):
-            data = int(input("enter ele to insert"))
+        for _ in range(int(input("Enter no of nodes : "))):
+            data = int(input("enter ele to insert : "))
             self.insert_at_end(data)
     
     def insert_after(self,data,x):
@@ -154,36 +155,157 @@ class SingleLinkedList:
         while p.link.link is not None:
             p= p.link
         p.link=None
+
+    def reverse_list(self):
+        if self.start is None:
+            print('List is Empty')
+            return
+        if self.start.link is None:
+            print("list reversed")
+            return
+        prev = None
+        p=self.start
+        while p is not None:
+            nextNode = p.link
+            p.link=prev
+            prev=p
+            p=nextNode
+        self.start = prev
+        print("list reversed")
     
     def bubble_sort_exdata(self):
-        pass
+        p = self.start
+        while p is not None:
+            q=p.link
+            while q is not None:
+                if p.info>q.info:
+                    temp=p.info
+                    p.info = q.info
+                    q.info=temp
+                q=q.link
+            p=p.link
+        
     
     def bubble_sort_exlinks(self):
-        pass
-    
+        p = self.start
+        r = self.start
+        while p is not None:
+            q=p.link
+            while q is not None:
+                if p.info>q.info:
+                    p.link=q.link
+                    q.link=p
+                    if p!=self.start:
+                        r.link = q
+                    else:
+                        self.start=q
+                    p,q=q,p
+                q=q.link
+            r=p
+            p=p.link
+
+    def merge_sorted(self,li2):
+        p1 = self.start
+        p2= li2.start
+        if p1.info <= p2.info:
+            startM=p1
+            p1=p1.link
+        else:
+            startM=p2
+            p2=p2.link
+        pm = startM
+        while p1 is not None and p2 is not None:
+            if p1.info <= p2.info:
+                pm.link = p1
+                pm = pm.link
+                p1=p1.link
+            else:
+                pm.link = p2
+                pm = pm.link
+                p2=p2.link
+
+        if p1 is None:
+            pm.link=p2
+        else:
+            pm.link=p1
+        return startM
+
+    def mergesort(self,list_start):
+        if list_start is None or list_start.link is None:
+            return list_start
+
+        start1 = list_start
+        start2 = self.divide_list(list_start)
+
+        start1 = self.mergesort(start1)
+        start2 = self.mergesort(start2)
+        startm = self.mergelist(start1,start2)
+
+        return startm
+
+    def mergelist(self,p1,p2):
+        if p1.info<=p2.info:
+            startm=p1
+            p1=p1.link
+        else:
+            startm=p2
+            p2=p2.link
+        pm = startm
+        while p1 is not None and p2 is not None:
+            if p1.info <=p2.info:
+                pm.link = p1
+                pm=pm.link
+                p1=p1.link
+            else:
+                pm.link = p2
+                pm=pm.link
+                p2=p2.link
+        if p1 is None:
+            pm.link = p2
+        else:
+            pm.link = p1
+
+        return startm
+
+    def divide_list(self,p):
+        q=p.link.link
+        while q is not None and q.link is not None:
+            p=p.link
+            q=q.link.link
+        start2 = p.link
+        p.link = None
+        return start2
+
+    def insert_cycle(self,key):
+        if self.start is None or self.start.link is None:
+            print("Not able to insert cycle")
+            return
+        p=self.start
+        flag=0
+        while p.link is not None:
+            if p.info == key:
+                flag=1
+                inter = p
+            p=p.link
+        if flag==1:
+            p.link = inter
+        else:
+            print("Element Not Detected")
+
     def has_cycle(self):
-        pass
-    
-    def remove_cycle(self):
-        pass
-    
-    def insert_cycle(self,x):
-        pass
-    
-    def merge2(self,list2):
-        pass
-    
-    def _merge2(self,p1,p2):
-        pass
-    
-    def merge_sort(self):
-        pass
-    
-    def _merge_sort_rec(self,listStart):
-        pass
-    
-    def _divide_list(self,listStart):
-        pass
+        p=self.start
+        q=self.start
+        while p is not None and q is not None:
+            p=p.link
+            q=q.link.link
+            if p==q:
+                r=self.start
+                while r is not p:
+                    r=r.link
+                    p=p.link
+                    print("cycle detected at ele ",p.info)
+                return
+        print("No cycle Found")
         
 
 li=SingleLinkedList()
@@ -201,7 +323,15 @@ while True:
     print("9.delete first node")
     print("10.delete last node")
     print("11.delete node")
-    option = int(input('enter your option :'))
+    print("12.Reverse a List")
+    print("13.bubble sort by exchanging data")
+    print("14 bubble sort by exchanging links")
+    print("15 merge two sorted list")
+    print("16 merge sort")
+    print("17 insert cycle")
+    print("18 detect cycle")
+    
+    option = int(input('enter your option : '))
     if option == 1:
         li.display_list()
     elif option == 2:
@@ -231,5 +361,33 @@ while True:
     elif option == 11:
         ele = int(input('enter the ele to delete :'))
         li.delete_node(ele)
+    elif option == 12:
+        li.reverse_list()
+    elif option == 13:
+        li.bubble_sort_exdata()
+        print("list sorted")
+    elif option == 14:
+        li.bubble_sort_exdata()
+        print("list sorted")
+    elif option == 15:
+        li1=SingleLinkedList()
+        li2=SingleLinkedList()
+        resultList = SingleLinkedList() 
+        print("Enter list 1")
+        li1.create_list()
+        li1.bubble_sort_exdata()
+        print("Enter list 2")
+        li2.create_list()
+        li2.bubble_sort_exdata()
+        li1.start=li1.merge_sorted(li2)
+        li=li1
+    elif option == 16:
+        li.start = li.mergesort(li.start)
+        print("list sorted")
+    elif option == 17:
+        ele  = int(input('enter ele : '))
+        li.insert_cycle(ele)
+    elif option == 18:
+        li.has_cycle()
     else:
         break
